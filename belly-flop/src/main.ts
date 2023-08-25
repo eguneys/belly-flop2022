c4ac0a02f4f9d16cc3fa864e1bfea59e4ed8a81c
eguneys <eguneys@gmail.com>
2023-08-25 17:29:03 +0000

    saturated colors, wave functions

--- belly-flop/src/main.ts
+++ belly-flop/src/main.ts
@@ -110,7 +110,7 @@ class Gg {
   }
 
   dot(x: number, y: number, r: number = 100) {
-    this.cx.arc(x, y, r, 0, pi)
+    this.cx.arc(x, y, r, 0, pi2)
   }
 
   bp(w: number = 8) {
@@ -185,6 +185,48 @@ const yllw = '#dacf80'
 const grn = '#60e331'
 const blu = '#319ae3'
 
+/* https://chat.openai.com/share/f4ff1e1b-f9e4-468f-8b7d-1c05ddd20dde */
+function adjustSaturation(originalColorHex, saturationChange) {
+    // Convert the original color to an RGBA format
+    const r = parseInt(originalColorHex.substring(1, 3), 16);
+    const g = parseInt(originalColorHex.substring(3, 5), 16);
+    const b = parseInt(originalColorHex.substring(5, 7), 16);
+    const a = 1; // Alpha channel (fully opaque)
+
+    // Calculate the adjusted color by shifting the RGB values
+    const adjustedR = Math.min(255, Math.max(0, Math.round(r + (r * saturationChange))));
+    const adjustedG = Math.min(255, Math.max(0, Math.round(g + (g * saturationChange))));
+    const adjustedB = Math.min(255, Math.max(0, Math.round(b + (b * saturationChange))));
+
+    // Create the adjusted color in RGBA format
+    const adjustedColor = `rgba(${adjustedR}, ${adjustedG}, ${adjustedB}, ${a})`;
+
+    return adjustedColor;
+}
+
+const _sat = (c: string) => adjustSaturation(c, 0.8)
+const _l_sat = (c: string) => adjustSaturation(c, -0.8)
+
+const l_bgn = _l_sat('#1e1b22')
+const l_red = _l_sat('#5d1320')
+const l_prpl = _l_sat('#7431e3')
+const l_lprpl = _l_sat('#e331da')
+const l_lyllw = _l_sat('#e3cd31')
+const l_yllw = _l_sat('#dacf80')
+const l_grn = _l_sat('#60e331')
+const l_blu = _l_sat('#319ae3')
+
+const m_bgn = _sat('#1e1b22')
+const m_red = _sat('#5d1320')
+const m_prpl = _sat('#7431e3')
+const m_lprpl = _sat('#e331da')
+const m_lyllw = _sat('#e3cd31')
+const m_yllw = _sat('#dacf80')
+const m_grn = _sat('#60e331')
+const m_blu = _sat('#319ae3')
+
+
+
 
 const gor = 1.61803;
 const epsi = 1e-5;
@@ -220,10 +262,6 @@ function app(e: HTMLElement) {
   }
 }
 
-function sm_sin(v: number, f = 0.003) {
-  return Math.sin(life * f) * v
-}
-
 function sum(a: number[]) {
   return a.reduce((a, b) => a + b, 0)
 }
@@ -234,19 +272,76 @@ const juicyNoise = (x: number) => {
     return (sinValue + cosValue) / 2;
 };
 
+
+const lin = (x: number) => x
+const sin = (x: number) => Math.sin(x)
+const cos = (x: number) => Math.cos(x)
+const sqr = (x: number) => x < 0.5 ? 0 : 1
+const saw = (x: number) => x % 0.5
+
+const _mod_13 = (f: (x: number) => number) => (x: number) => f(x % 13 / 13)
+
+const e_lin = _mod_13((x: number) => x)
+const e_sin = _mod_13((x: number) => sin(x * pi2))
+const e_cos = _mod_13((x: number) => cos(x * pi2))
+const e_sqr = _mod_13((x: number) => sqr(x))
+const e_saw = _mod_13((x: number) => saw(x))
+
+
+const hi = false
+const lo = false
+const hi_lo = <A>(h: A, m: A, l: A) => hi ? h : lo ? l : m
+
+const rnd19 = () => Math.random() * 19
+
+const _rnd_13 = <A>(f: (v: number) => A) => f(rnd19() % 13 / 13)
+
+const fls = <A>(h: A, l: A) => _rnd_13((v: number) => (life * 0.000003 % v) / (v * 1.2) < 0.5 ? h : l)
+
 function loop(m: Mm, g: Gg) {
 
-  let x_brsh = [...Array(10).keys()].map(i => juicyNoise(i))
-  let y_brsh = [...Array(10).keys()].map(i => juicyNoise(30 - i))
+  let u_angle = sin(life * 0.003) * h64p
+  let u_s_f40 = sin(life * 0.02) * h64p
 
-  let u_angle = sm_sin(h64p)
-  let u_s_f40 = sm_sin(h64p, 0.02)
 
+  /* Background */
 
   g.fc(bgn)
   g.fr(0, 0, w, h)
 
-  g.sc(red)
+  let r = 40
+
+
+  g.sc(l_lyllw)
+  g.bp(4)
+  for (let i = 0; i < 14; i++) {
+    g.dot(500 + e_sin(i+u_angle * u_angle * 3000) * r, 600 + e_cos(-i/2) * r * 2, 10)
+  }
+  g.ep()
+
+
+
+  g.sc(l_blu)
+  g.bp(4)
+  for (let i = 0; i < 14; i++) {
+    g.dot(hw + e_cos(i) * e_cos(u_angle * 10) * r, hh + e_sin(i) * u_angle * 10 * r * 2, 10)
+  }
+  g.ep()
+
+
+  g.sc(_l_sat('#cdcddc'))
+  g.bp(3)
+  for (let i = 1; i < 13; i++) {
+    g.dot(100 + e_sqr(i + life * 0.003) * 100, 0 + e_lin(i) * 1080, 8)
+  }
+  g.ep()
+
+
+  /* Foreground */
+
+  const hi_lo_red = hi_lo(m_red, red, l_red)
+
+  g.sc(hi_lo_red)
   g.bp()
 
   let a = 38, a3 = a * 3, a8 = a * 8
@@ -267,8 +362,8 @@ function loop(m: Mm, g: Gg) {
 
 
   let [btn_x, btn_y] = [hw - h2w - h4w, hh + 2 * h3h]
-  let hovering2 = m.hovering(btn_x, btn_y, 320)
-  let hovering = m.hovering(btn_x, btn_y, 280)
+  let hovering2 = m.hovering(btn_x, btn_y, 360)
+  let hovering = m.hovering(btn_x, btn_y, 120)
   let color = hovering ? red : yllw
 
   g.fc(yllw)
@@ -282,13 +377,15 @@ function loop(m: Mm, g: Gg) {
     g.str('c', 50 + u_s_f40 * 40, -30)
     g.str('e', 140 + u_s_f40 * 40, -30)
   } else if (hovering2) {
+    g.fc(m_yllw)
+    g.str('dice', 0, 0)
   } else {
     g.str('dice', 0, 0)
   }
   g.rsto()
 
-  g.fc(yllw)
-  g.sc(yllw)
+  g.fc(l_yllw)
+  g.sc(fls(l_yllw, yllw))
   g.save()
   g.rotate(-h8p + u_angle, hw + h3w + h8w, hh + 2 * h3h)
   g.sr(300, 100)
@@ -303,7 +400,6 @@ function loop(m: Mm, g: Gg) {
   g.rotate(0, hw - a8 - a3, hh + a)
   g.fr(0, 0, 600, 100)
   g.rsto()
-
 
 
 
@@ -331,12 +427,11 @@ function loop(m: Mm, g: Gg) {
 
   g.ep()
 
-  g.fc(blu)
+  g.fc(m_blu)
   g.lrg()
   g.str('belly basket', hw, 0)
 
 
-  let r = 40
   g.save()
   g.sc(red)
   g.bp()
@@ -350,22 +445,7 @@ function loop(m: Mm, g: Gg) {
   }
   g.ep()
   g.rsto()
-
-
-  g.sc(blu)
-  g.bp(4)
-  x_brsh.forEach(x =>
-    y_brsh.forEach(y => {
-      let r = 100
-      g.m2(hw + x * r, hh + y * r)
-      g.dot(hw + x * r, hh + y * r, 10)
-    }))
-  g.ep()
-
-
   
-
-
 }
 
 
